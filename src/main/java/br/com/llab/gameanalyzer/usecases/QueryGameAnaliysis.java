d17eae0f00c156a5e69181d8c31d326a080b4eee
cleber Santaterra <cleberms@ciandt.com>
2019-04-13 18:41:35 -0300

    added method to query an game by game number

---
+++ src/main/java/br/com/llab/gameanalyzer/usecases/QueryGameAnaliysis.java
@@ -0,0 +1,23 @@
+package br.com.llab.gameanalyzer.usecases;
+
+import br.com.llab.gameanalyzer.domains.Game;
+import br.com.llab.gameanalyzer.gateways.GameDatabaseGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QueryGameAnaliysis {
+
+    @Autowired
+    private GameDatabaseGateway gateway;
+
+    public List<Game> query() {
+        return gateway.getAll();
+    }
+
+    public List<Game> query(final String gameNumber) {
+        return gateway.getAll();
+    }
+}
